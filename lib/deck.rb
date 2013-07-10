d3d82b8b5ea8578bba083c82d651fb6c2d6c8e01
Michael Pope <amorphid@captaincoder.io>
2013-07-09 17:30:39 -0700

    Replace all def foo;@foo;end methods with attr_reader :foo

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,10 @@
 require_relative "card"
 
 class Deck
+  attr_reader :cards
+
   def initialize()
     @cards = []
     52.times { @cards << Card.new }
-  end
-
-  def cards
-    @cards
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -2,21 +2,13 @@ require_relative "deck"
 require_relative "player"
 
 class Game
+  attr_reader :dealer,
+              :deck,
+              :player
+
   def initialize
     @dealer = Dealer.new
     @deck   = Deck.new
     @player = Player.new
-  end
-
-  def dealer
-    @dealer
-  end
-
-  def deck
-    @deck
-  end
-
-  def player
-    @player
   end
 end
