86349af6e774eee2c5fbef54d7cdde9894623065
Michael Pope <amorphid@captaincoder.io>
2013-07-10 19:30:21 -0700

    Gutted game class and deleted useless tests.  Feels good :)

--- lib/classes/game.rb
+++ lib/classes/game.rb
@@ -1,18 +1,9 @@
 class Game
-  attr_reader :dealer,
-              :deck,
-              :player
-
   def initialize
-    @dealer = Dealer.new
     @deck   = Deck.new
     @player = Player.new
+    @dealer = Dealer.new
     @deck.shuffle!
-  end
-
-  def deal_first_hand
-    2.times { puts @player.hand.cards << @deck.cards.pop }
-    2.times { @player.hand.cards << @deck.cards }
   end
 end
 
--- spec/classes/game_spec.rb
+++ spec/classes/game_spec.rb
@@ -1,37 +1,5 @@
 require "spec_helper"
 
 describe Game do
-  let(:game) { Game.new }
-
-  describe "#deck" do
-    let(:deck) { game.deck }
-
-    it "should be an Deck" do
-      expect(deck).to be_a(Deck)
-    end
-
-    it "should be shuffled" do
-      shuffled = deck.cards
-      not_shuffled = Deck.new
-
-      expect(shuffled).not_to eq(not_shuffled)
-    end
-  end
-
-  describe "#dealer" do
-    it "should create a Dealer" do
-      dealer = game.dealer
-
-      expect(dealer).to be_a(Dealer)
-    end
-  end
-
-  describe "#player" do
-    it "should create a Player" do
-      player = game.player
-
-      expect(player).to be_a(Player)
-    end
-  end
 end
 
