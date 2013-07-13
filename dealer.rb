4df584cab04385d3b32ab4d20df351aecbc71113
Michael Pope <amorphid@captaincoder.io>
2013-07-12 22:13:20 -0700

    Create dealer#deal

--- dealer.rb
+++ dealer.rb
@@ -3,5 +3,11 @@ require "./deck.rb"
 class Dealer
   def initialize
     @deck = Deck.new
+    @deck.shuffle!
+    deal(self, 2)
+  end
+
+  def deal(player, n = 1)
+    # need to implement Player#hand
   end
 end
