b5e0d42038a80a40c4da3fef567763a2e0a3ce1c
Michael Pope <amorphid@captaincoder.io>
2013-07-12 21:55:49 -0700

    Add #shuffle, #shuffle!, and @cards to Deck

--- deck.rb
+++ deck.rb
@@ -1,6 +1,34 @@
+require "./card.rb"
+
 class Deck
-  attr_reader :blackjack
+  attr_reader :blackjack,
+              :shuffle
+
+  def initialize
+    @cards = []
+  end
 
   def blackjack
+    ranks.product(suits).each do |rank, card|
+      @cards << Card.new(rank, card)
+    end
+
+    @cards
+  end
+
+  def ranks
+    "2".upto("9").to_a + ["T", "J", "Q", "K", "A"]
+  end
+
+  def shuffle
+    @cards.shuffle
+  end
+
+  def shuffle!
+    @cards = shuffle.dup!
+  end
+
+  def suits
+    ["C", "D", "H", "S"]
   end
 end
